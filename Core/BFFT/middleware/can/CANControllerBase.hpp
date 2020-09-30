813d01a78f06672be910928f9c67ea5c7e959d0a
Alexander Bierenstiel <bierenstiel.alexander@gmail.com>
2020-09-30 20:12:13 +0200

    added interface and base class for CAN controller

---
+++ Core/BFFT/middleware/can/CANControllerBase.hpp
@@ -0,0 +1,43 @@
+/*
+ * CANControllerBase.hpp
+ *
+ *  Created on: 30.09.2020
+ *      Author: Alex
+ */
+
+#ifndef BFFT_MIDDLEWARE_CAN_CANCONTROLLERBASE_HPP_
+#define BFFT_MIDDLEWARE_CAN_CANCONTROLLERBASE_HPP_
+
+#include <ICANController.hpp>
+#include <vector>
+
+
+class CANControllerBase : public ICANController
+{
+	std::vector<CANFrameId> _canFrameIds;
+	std::vector<ICANSubscriber*> _canSubscribers;
+
+public:
+	void CANControllerBase::addSubscriber(CANFrameId id, ICANSubscriber &subscriber) override
+	{
+		_canFrameIds.push_back(id);
+		_canSubscribers.push_back(&subscriber);
+	}
+
+	void CANControllerBase::receive(CANFrame frame) override
+	{
+		// update each subscriber that listens to the same frame id as the id of the given frame
+		for (auto it = _canFrameIds.begin(); it != _canFrameIds.end(); ++it)
+		{
+			if (frame.id == *it)
+			{
+				const int index = it - _canFrameIds.begin();
+				ICANSubscriber *subscriber = _canSubscribers[index];
+				subscriber->receive(frame);
+			}
+		}
+	}
+};
+
+
+#endif /* BFFT_MIDDLEWARE_CAN_CANCONTROLLERBASE_HPP_ */
---
+++ Core/BFFT/middleware/can/ICANController.hpp
@@ -0,0 +1,25 @@
+/*
+ * ICANController.hpp
+ *
+ *  Created on: 30.09.2020
+ *      Author: Alex
+ */
+
+#ifndef BFFT_MIDDLEWARE_CAN_ICANCONTROLLER_HPP_
+#define BFFT_MIDDLEWARE_CAN_ICANCONTROLLER_HPP_
+
+#include <CANFrameId.hpp>
+#include <CANFrame.hpp>
+#include <ICANSubscriber.hpp>
+
+
+class ICANController
+{
+public:
+	virtual void addSubscriber(CANFrameId id, ICANSubscriber &subscriber);
+	virtual void receive(CANFrame frame);
+	virtual void send(CANFrame frame);
+};
+
+
+#endif /* BFFT_MIDDLEWARE_CAN_ICANCONTROLLER_HPP_ */
