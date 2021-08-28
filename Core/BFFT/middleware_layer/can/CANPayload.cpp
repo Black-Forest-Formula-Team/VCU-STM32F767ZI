cf9fdac0b5504a33fbd46f317dd25b5993ffa111
hegdekrishna <krishnahegde007@gmail.com>
2021-08-28 15:26:42 +0200

    Clean the code CanPayload

--- Core/BFFT/middleware_layer/can/CANPayload.cpp
+++ Core/BFFT/middleware_layer/can/CANPayload.cpp
@@ -12,13 +12,13 @@
 
 bool CANPayload::isValid() const
 {
-	if (isRemoteFrame && bitLength) return false;
+	if ((isRemoteFrame) && (bitLength)) return false;
 	if (bitLength > 64) return false;
 	if (bitLength <= 32)
 	{
 		if (data.uint32[1]) return false;
 		if (data.uint32[0] >> bitLength) return false;
 	}
-	else if (data.uint32[1] >> (bitLength - 32)) return false;
+	else if ((data.uint32[1]) >> (bitLength - 32)) return false;
 	return true;
 }
